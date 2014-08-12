fb2e546fc3ce1faa5d979017cf8aacba5dc26ff5
Alex Grant <grantovich@users.noreply.github.com>
2014-08-12 13:35:30 -0400

    Reconfigure environments for memory caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,13 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Show full error reports
+  config.consider_all_requests_local = true
+
+  # For testing caching in development
+  # (use `false` and `:null_store` respectively to disable)
+  config.action_controller.perform_caching = true
+  config.cache_store = :memory_store
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Prevent low-level caching in tests
+  config.cache_store = :null_store
+
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
